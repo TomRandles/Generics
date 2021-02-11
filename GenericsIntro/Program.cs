533f4af25e5586be5a75303d394cafc1069c7de8
Tom Randles <randles.tom@gmail.com>
2021-02-11 17:02:24 +0000

    Add project files.

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,44 @@
+﻿using GenericsIntro.Domain;
+using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var buffer = new Buffer<double>();
+
+            ProcessInput(buffer);
+            ProcessBuffer(buffer);
+        }
+
+        private static void ProcessBuffer(Buffer<double> buffer)
+        {
+            var sum = 0.0;
+            Console.WriteLine("Buffer: ");
+            while (!buffer.IsEmpty)
+            {
+                sum += buffer.Read();
+            }
+            Console.WriteLine(sum);
+        }
+
+        private static void ProcessInput(Buffer<double> buffer)
+        {
+            while (true)
+            {
+                var value = 0.0;
+                var input = Console.ReadLine();
+
+                if (double.TryParse(input, out value))
+                {
+                    buffer.Write(value);
+                    continue;
+                }
+                break;
+            }
+        }
+    }
+}
---
+++ GenericsIntro/Tests/BufferTests.cs
@@ -0,0 +1,31 @@
+﻿using GenericsIntro.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+namespace GenericsIntro.Tests
+{
+
+    [TestClass]
+    public class BufferTests
+    {
+        [TestMethod]
+        public void CheckBufferIsEmpty()
+        {
+            var buffer = new Buffer<double>();
+
+            Assert.IsTrue(buffer.IsEmpty);
+        }
+
+        [TestMethod]
+        public void CheckBufferIsFull()
+        {
+            var buffer = new Buffer<double>(capacity: 3);
+            buffer.Write(1.5);
+            buffer.Write(1.5);
+            buffer.Write(1.5);
+
+            Assert.IsTrue(buffer.IsFull);
+        }
+    }
+}
